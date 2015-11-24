746f4086332cb591d66fc938551a40d04c8cb042
Bogdan Cochior <bogdan.cochior@thinslices.com>
2015-11-24 12:19:12 +0200

    Upload build on github

--- src/client/app/administratorModule/administrator.module.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    'use strict';
-
-    angular.module('app.administrator', [
-        'app.core',
-        'app.widgets'
-    ]);
-})();
--- src/client/app/ownerModule/owner.module.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    'use strict';
-
-    angular.module('app.owner', [
-        'app.core',
-        'app.widgets'
-    ]);
-})();
